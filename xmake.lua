9c71284bb727b2ae9c39c31521325a723e1470a3
Xingyu <1773362692@qq.com>
2023-02-17 20:51:08 +0800

    update xmake lua

--- xmake.lua
+++ xmake.lua
@@ -1,7 +1,12 @@
 add_rules("mode.debug", "mode.release")
 add_includedirs("source/")
+set_languages("cxx20")
 
-target("main")
-    set_languages("cxx20")
+target("JsonParser")
+    set_kind("static")
+    add_files("source/json/**.cpp")
+
+target("Test")  
     set_kind("binary") 
-    add_files("source/**.cpp")
+    add_files("source/*.cpp")
+    add_deps("JsonParser")
